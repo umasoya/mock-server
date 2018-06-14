98d8d2bc43b76673e6667e709f002b7983c0f105
umasoya <basilisk.0331@icloud.com>
2018-06-14 13:13:35 +0900

    to ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
-var gulp = require('gulp');
-var ejs  = require('gulp-ejs');
-var fs   = require('fs');
+const fs   = require('fs');
+const gulp = require('gulp');
+const ejs  = require('gulp-ejs');
+const sass = require('gulp-sass');
 
-gulp.task('ejs', function() {
-    var json = JSON.parse(fs.readFileSync('./json/config.json'));
+gulp.task('ejs', () => {
+    const json = JSON.parse(fs.readFileSync('./json/config.json'));
     gulp.src('ejs/**/*.ejs')
-    .pipe(ejs(json, {}, {"ext": ".html"}))
-    .pipe(gulp.dest('public'))
+        .pipe(ejs(json, {}, {"ext": ".html"}))
+        .pipe(gulp.dest('public'))
 });
+
+gulp.task('sass', () => {
+    gulp.src('scss/**/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('ejs/**/*.ejs', ['ejs']);
+});
+
+gulp.task('default', ['ejs', 'sass']);
