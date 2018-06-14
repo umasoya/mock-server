dba4cee273aa596fb049133e500cc7fd5d1ffa2f
umasoya <basilisk.0331@icloud.com>
2018-06-14 15:52:26 +0900

    browser-sync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,26 +1,58 @@
 'use strict';
 
-import fs   from 'fs';
-import gulp from 'gulp';
-import ejs  from 'gulp-ejs';
-import sass from 'gulp-sass';
+import fs          from 'fs';
+import gulp        from 'gulp';
+import ejs         from 'gulp-ejs';
+import sass        from 'gulp-sass';
+import browserSync from 'browser-sync';
 
+/*** consts ***/
+const path = {
+    publicDir: 'public/',
+    jsonFile: 'json/config.json',
+
+    ejs: {src: 'ejs/**/*.ejs', dest: 'public/'},
+    sass: {src: 'scss/**/*.scss', dest: 'public/css/'}
+};
+
+const ejsOptions = {
+    "ext": ".html"
+};
+
+const sassOptions = {
+    outputStyle: 'expanded'
+};
+
+
+/*** tasks ***/
 gulp.task('ejs', () => {
-    const json = JSON.parse(fs.readFileSync('./json/config.json'));
-    gulp.src('ejs/**/*.ejs')
-        .pipe(ejs(json, {}, {"ext": ".html"}))
-        .pipe(gulp.dest('public'))
+    const json = JSON.parse(fs.readFileSync(path.jsonFile));
+    gulp.src(path.ejs.src)
+        .pipe(ejs(json, {}, ejsOptions))
+        .pipe(gulp.dest(path.ejs.dest))
 });
 
 gulp.task('sass', () => {
-    gulp.src('scss/**/*.scss')
-        .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('public/css/'));
+    gulp.src(path.sass.src)
+        .pipe(sass(sassOptions))
+        .pipe(gulp.dest(path.sass.dest));
 });
 
-gulp.task('watch', () => {
-    gulp.watch('scss/**/*.scss', ['sass']);
-    gulp.watch('ejs/**/*.ejs', ['ejs']);
+gulp.task('watch', ['browserSync-init'], () => {
+    gulp.watch(path.sass.src, ['sass', 'reload']);
+    gulp.watch(path.ejs.src, ['ejs', 'reload']);
+});
+
+gulp.task('browserSync-init', () => {
+    browserSync.init({
+        server: {
+            baseDir: path.publicDir
+        }
+    });
+});
+
+gulp.task('reload', () => {
+    browserSync.reload();
 });
 
 gulp.task('default', ['ejs', 'sass']);
