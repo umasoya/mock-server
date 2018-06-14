8d869e6addcfa9ffa7b578969174e85789575ba9
umasoya <basilisk.0331@icloud.com>
2018-06-14 10:47:25 +0900

    load json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
-var ejs = require('gulp-ejs');
+var ejs  = require('gulp-ejs');
+var fs   = require('fs');
 
 gulp.task('ejs', function() {
+    var json = JSON.parse(fs.readFileSync('./json/config.json'));
     gulp.src('ejs/**/*.ejs')
-    .pipe(ejs({},{},{"ext": ".html"}))
+    .pipe(ejs(json, {}, {"ext": ".html"}))
     .pipe(gulp.dest('public'))
 });
